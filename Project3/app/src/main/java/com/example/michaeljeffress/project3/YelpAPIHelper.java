d6e962dda8148658d9e227391c5a14a1b423c4d0
Finnegan1441 <patrickcummins2009@gmail.com>
2016-08-02 14:50:29 -0700

    Yelp API now works, Current Location doesn't work for tablets and emulators for some reason

--- Project3/app/src/main/java/com/example/michaeljeffress/project3/YelpAPIHelper.java
+++ Project3/app/src/main/java/com/example/michaeljeffress/project3/YelpAPIHelper.java
@@ -7,10 +7,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.yelp.clientlib.connection.YelpAPI;
 import com.yelp.clientlib.connection.YelpAPIFactory;
@@ -19,6 +22,7 @@ import com.yelp.clientlib.entities.Coordinate;
 import com.yelp.clientlib.entities.SearchResponse;
 import com.yelp.clientlib.entities.options.CoordinateOptions;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
 
@@ -26,136 +30,66 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
 import static android.view.View.Y;
 import static com.example.michaeljeffress.project3.R.id.textView;
+import static com.example.michaeljeffress.project3.R.string.location;
+import static com.google.android.gms.analytics.internal.zzy.b;
+import static com.google.android.gms.analytics.internal.zzy.t;
 
 /**
  * Created by patrickcummins on 8/1/16.
  */
 
-public class YelpAPIHelper implements
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class YelpAPIHelper {
     String consumerKey, consumerSecret, token, tokenSecret;
     YelpAPI yelpAPI;
-    ArrayList<Business> businesses = new ArrayList<>();
-    GoogleApiClient mGoogleApiClient;
-    Location mLocation;
     Context context;
+    ArrayList<Business> nearbyBuisnesses;
+    private static OnResponseFinished onResponseFinished;
 
-    public YelpAPIHelper(Context context) {
-        consumerKey = "M1_SfyvyDc5Ra0ULuy6D8A";
-        consumerSecret = "HS72vU6uMyXlERolPnGAY6gab3s";
-        token = "pmNtphuK3EzosXgDbU4j6CDhozciqqUR";
-        tokenSecret = "eSy2yUWHoA2fSKg0fYvpKEslMG8";
+    //boolean to check retrofit synchonisity
+    public interface OnResponseFinished {
+        public void onBuisnessesRecieved(ArrayList<Business> buisnesses);
+
+    }
+
+
+    public YelpAPIHelper(Context context, OnResponseFinished onResponseFinished) {
+        consumerKey = "";
+        consumerSecret = "";
+        token = "";
+        tokenSecret = "";
         YelpAPIFactory apiFactory = new YelpAPIFactory(consumerKey, consumerSecret, token, tokenSecret);
         yelpAPI = apiFactory.createAPI();
+        this.onResponseFinished = onResponseFinished;
         this.context = context;
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(context).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
-        }
-        mGoogleApiClient.connect();
     }
 
-    //Gets Names of Buisnesses
-    public ArrayList<String> getBuisnessesNames(Map<String, String> params) {
-        Call<SearchResponse> call = yelpAPI.search("San Francisco", params);
-        final ArrayList<String> businessNames = new ArrayList<>();
-        Callback<SearchResponse> callback = new Callback<SearchResponse>() {
-            @Override
-            public void onResponse(Call<SearchResponse> call, Response<SearchResponse> response) {
-                SearchResponse searchResponse = response.body();
-                ArrayList<Business> businesses = searchResponse.businesses();
-                for (int i = 0; i < businesses.size(); i++) {
-                    businessNames.add(businesses.get(i).name());
-                }
-
-            }
-
-            @Override
-            public void onFailure(Call<SearchResponse> call, Throwable t) {
-
-            }
-        };
-        call.enqueue(callback);
-        return businessNames;
-    }
 
     //GetsBuisnesses by Params
-    public ArrayList<Business> getBusinesess(Map<String, String> params) {
-        Call<SearchResponse> call = yelpAPI.search("San Francisco", params);
+    public void getBusinesess(Map<String, String> params, Location location) {
+        CoordinateOptions coordinateOptions = CoordinateOptions.builder().latitude(location.getLatitude()).longitude(location.getLongitude()).build();
 
-        Callback<SearchResponse> callback = new Callback<SearchResponse>() {
+        Log.d("lat and lon for Busis", location.getLatitude() + " ," + location.getLongitude());
+        Call<SearchResponse> call = yelpAPI.search(coordinateOptions, params);
 
-            @Override
-            public void onResponse(Call<SearchResponse> call, Response<SearchResponse> response) {
-                SearchResponse searchResponse = response.body();
-                businesses = searchResponse.businesses();
+        final ArrayList<Business> businesses = new ArrayList<>();
 
 
-            }
-
-            @Override
-            public void onFailure(Call<SearchResponse> call, Throwable t) {
-
-            }
-        };
-        call.enqueue(callback);
-        return businesses;
-
-    }
-
-    //GetsBuisnesses near by
-    public ArrayList<Business> getBusinesessNearby(Map<String, String> params) {
-        params.put("latitude", Double.toString(mLocation.getLatitude()));
-        params.put("longitude", Double.toString(mLocation.getLongitude()));
-        Call<SearchResponse> call = yelpAPI.search("San Francisco", params);
-
-
-        Callback<SearchResponse> callback = new Callback<SearchResponse>() {
+        call.enqueue(new Callback<SearchResponse>() {
 
             @Override
             public void onResponse(Call<SearchResponse> call, Response<SearchResponse> response) {
                 SearchResponse searchResponse = response.body();
-                businesses = searchResponse.businesses();
-
-
+                nearbyBuisnesses = searchResponse.businesses();
+                onResponseFinished.onBuisnessesRecieved(nearbyBuisnesses);
             }
 
             @Override
             public void onFailure(Call<SearchResponse> call, Throwable t) {
-
+                Toast.makeText(context, "Failed To Connect", Toast.LENGTH_SHORT).show();
             }
-        };
-        call.enqueue(callback);
-        return businesses;
-
-    }
-
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-
-        }
-        mLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        });
     }
 }
