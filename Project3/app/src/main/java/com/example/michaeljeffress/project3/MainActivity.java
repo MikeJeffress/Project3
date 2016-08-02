dd75bca7df73cc472768f84e45733d08a7409138
Audrey So <audreyeso@Audreys-MacBook-Air.local>
2016-08-02 16:26:28 -0700

    adds onBusinessReceived to weatherbusiness activity

--- Project3/app/src/main/java/com/example/michaeljeffress/project3/MainActivity.java
+++ Project3/app/src/main/java/com/example/michaeljeffress/project3/MainActivity.java
@@ -1,27 +1,25 @@
 package com.example.michaeljeffress.project3;
 
-import android.content.Intent;
-import android.location.Location;
-
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentActivity;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -34,32 +32,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
-import retrofit2.http.HEAD;
-
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-import com.yelp.clientlib.entities.Business;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener, YelpAPIHelper.OnResponseFinished {
     GoogleApiClient mGoogleApiClient;
--- Project3/app/src/main/java/com/example/michaeljeffress/project3/WeatherBusinessActivity.java
+++ Project3/app/src/main/java/com/example/michaeljeffress/project3/WeatherBusinessActivity.java
@@ -3,11 +3,33 @@ package com.example.michaeljeffress.project3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class WeatherBusinessActivity extends AppCompatActivity {
+import com.yelp.clientlib.entities.Business;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class WeatherBusinessActivity extends AppCompatActivity implements YelpAPIHelper.OnResponseFinished{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_business);
+
+        YelpAPIHelper yelpAPIHelper = new YelpAPIHelper(WeatherBusinessActivity.this, WeatherBusinessActivity.this);
+        HashMap<String, String> hashMapParams = new HashMap<String, String>();
+        //yelpAPIHelper.getBusinesess(hashMapParams,currentLocation); //get location
+    }
+
+    @Override
+    public void onBuisnessesRecieved(ArrayList<Business> buisnesses) {
+        Business currentBusiness = buisnesses.get(0);
+        currentBusiness.name();
+        currentBusiness.imageUrl();
+        currentBusiness.mobileUrl();
+        currentBusiness.rating();
+        currentBusiness.reviews();
+        currentBusiness.isClosed();
+        currentBusiness.location().address();
+
     }
 }
